a4c1a9e92a31c30d2559ceb432090b288003f912
gatsbyx <gtx@gtx.ru>
2024-07-08 19:55:54 +0300

    ioctl, read, write fix

--- chardevice.c
+++ chardevice.c
@@ -11,6 +11,21 @@
 
 #define COMPAT_MODE
 
+struct CharDevice_ioctl_last_read {
+    pid_t pid;
+    long long time;
+};
+
+struct CharDevice_ioctl_last_write {
+    pid_t pid;
+    long long time;
+};
+
+#define CharDevice_IOCTL_MAGIC 'm'
+#define CharDevice_IOCTL_SET_BLOCKING _IOW(CharDevice_IOCTL_MAGIC, 0, int)
+#define CharDevice_IOCTL_GET_LAST_READ _IOR(CharDevice_IOCTL_MAGIC, 1, struct CharDevice_ioctl_last_read)
+#define CharDevice_IOCTL_GET_LAST_WRITE _IOR(CharDevice_IOCTL_MAGIC, 2, struct CharDevice_ioctl_last_write)
+
 static int BUF_SIZE = 1024;
 
 struct CharDevice {
@@ -57,7 +72,9 @@ static ssize_t CharDevice_read(struct file* file,
         wait_event_interruptible(dev->readQueue, dev->head != dev->tail);
     }
 
-    int available = (dev->head - dev->tail) % BUF_SIZE;
+    int available = (dev->head >= dev->tail) ? (dev->head - dev->tail)
+                                             : (BUF_SIZE - dev->tail + dev->head);
+    printk(KERN_DEBUG "Available memory %d\n", available);
     if(available == 0) {
         ret = 0;
         goto out;
@@ -97,7 +114,12 @@ static ssize_t CharDevice_write(struct file* file,
         wait_event_interruptible(dev->writeQueue,
                                  dev->head != (dev->tail + 1) % BUF_SIZE);
     }
-    int free = (BUF_SIZE + dev->tail - dev->head - 1) % BUF_SIZE;
+    int free;
+    if (dev->head <= dev->tail) {
+        free = BUF_SIZE - dev->tail + dev->head - 1;
+    } else {
+        free = dev->head - dev->tail - 1;
+    }
     if(free == 0) {
         // No space for new data
         printk(KERN_DEBUG "No memory in buffer for write\n");
@@ -129,7 +151,43 @@ out:
 }
 
 static long CharDevice_ioctl(struct file* file, unsigned int cmd, unsigned long arg) {
-    return 0;
+    struct CharDevice* dev = file->private_data;
+    int ret = 0;
+
+    mutex_lock(&dev->mutex);
+
+    switch (cmd) {
+        case CharDevice_IOCTL_SET_BLOCKING: {
+            dev->blocking = arg;
+            break;
+        }
+        case CharDevice_IOCTL_GET_LAST_READ: {
+            struct CharDevice_ioctl_last_read lastRead;
+            lastRead.pid = dev->lastReadPid;
+            lastRead.time =
+                dev->lastRead.tv_sec * 1000000000LL + dev->lastRead.tv_nsec;
+            if(copy_to_user((void __user*)arg, &lastRead, sizeof(lastRead))) {
+                ret = -EFAULT;
+            }
+            break;
+        }
+        case CharDevice_IOCTL_GET_LAST_WRITE: {
+            struct CharDevice_ioctl_last_write lastWrite;
+            lastWrite.pid = dev->lastWritePid;
+            lastWrite.time =
+                dev->lastWrite.tv_sec * 1000000000LL + dev->lastWrite.tv_nsec;
+            if(copy_to_user((void __user*)arg, &lastWrite, sizeof(lastWrite))) {
+                ret = -EFAULT;
+            }
+            break;
+        }
+        default: {
+            ret = -ENOIOCTLCMD;
+        }
+    }
+
+    mutex_unlock(&dev->mutex);
+    return ret;
 }
 
 static struct file_operations fOps = {
--- tests/test_chardevice.c
+++ tests/test_chardevice.c
@@ -3,7 +3,24 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/ioctl.h>
 #include <assert.h>
+
+struct CharDevice_ioctl_last_read {
+    pid_t pid;
+    long long time;
+};
+
+struct CharDevice_ioctl_last_write {
+    pid_t pid;
+    long long time;
+};
+
+#define CharDevice_IOCTL_MAGIC 'm'
+#define CharDevice_IOCTL_SET_BLOCKING _IOW(CharDevice_IOCTL_MAGIC, 0, int)
+#define CharDevice_IOCTL_GET_LAST_READ _IOR(CharDevice_IOCTL_MAGIC, 1, struct CharDevice_ioctl_last_read)
+#define CharDevice_IOCTL_GET_LAST_WRITE _IOR(CharDevice_IOCTL_MAGIC, 2, struct CharDevice_ioctl_last_write)
+
 
 void test_open(int fd) {
     printf("[test_open]: opened fd: %d\n", fd);
@@ -27,10 +44,19 @@ void test_read(int fd) {
     assert(!strncmp(buf, "5", 1));
 }
 
+void test_ioctl(int fd) {
+    struct CharDevice_ioctl_last_read lastRead;
+    if (ioctl(fd, CharDevice_IOCTL_GET_LAST_READ, &lastRead) < 0) {
+        perror("Failed to get last read time");
+        return;
+    }
+    printf("[test_ioctl]: last read by PID %d at time %lld\n", lastRead.pid, lastRead.time / 1000000000LL);
+}
 int main(void) {
     int fd = open("/dev/char",  O_RDWR);
     test_open(fd);
     test_write(fd);
     test_read(fd);
+    test_ioctl(fd);
     close(fd);
 }
